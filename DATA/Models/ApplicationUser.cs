bae944c987808f9c5b578fe0546050f1c648bee1
Signalls <ndubuisi.charles@decagon.dev>
2025-03-18 23:22:08 +0100

    Added models

---
+++ DATA/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿namespace DATA.Models
+{
+    public class ApplicationUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
