d49b4456f31966a5c1bc71788b6eae63a2ee9366
Signalls <ndubuisi.charles@decagon.dev>
2025-04-05 23:16:37 +0100

    Initial Commit

---
+++ CORE/ReportService/IReportService.cs
@@ -0,0 +1,14 @@
+﻿using DATA.DTO;
+using System.Threading.Tasks;
+
+namespace CORE.ReportService
+{
+    public interface IReportService
+    {
+        Task<ResponseDto> UpdateReport(string reportId, ReportRequestDto reportRequest);
+        Task<ResponseDto> DeleteReport(string reportId);
+        Task<ResponseDto> CreateReport(ReportRequestDto reportRequest);
+        Task<ResponseDto> GetReportByIdAsync(string reportId);
+        Task<ResponseDto> GetAllReports();
+    }
+}
---
+++ DATA/DTO/ReportRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using DATA.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace DATA.DTO
+{
+    public class ReportRequestDto
+    {
+        [Required]
+        public string Title { get; set; }  // Brief summary of the report
+
+        [Required]
+        public string Description { get; set; }  // Brief summary of the report
+
+        [Required]
+        public ItemCategory Category { get; set; } // Details of the lost/found item
+
+        [Required]
+
+        public ReportType Type { get; set; } // Enum: Lost or Found
+        [Required]
+        public ItemRequestDto Item { get; set; }
+
+
+        [Required]
+        public string UserId { get; set; } // Foreign Key to User
+    }
+}
